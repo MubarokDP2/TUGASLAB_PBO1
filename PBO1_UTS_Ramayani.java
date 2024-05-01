4644d1da4c017d2bef8edc65c8b56412b1c1c025
Mubarok Khan <114717494+MubarokDP2@users.noreply.github.com>
2024-05-01 07:11:56 +0700

    Add files via upload

---
+++ PBO1_UTS_Ramayani.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo1_uts_ramayani;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PBO1_UTS_Ramayani {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KontakManager Contactlist = new KontakManager();
+                Contactlist.setVisible(true);
+    }
+    
+}
